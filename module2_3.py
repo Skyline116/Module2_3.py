303a5d0ca2ebff5fb3567341ddf55e16e8d864a4
Skyline116 <167173599+Skyline116@users.noreply.github.com>
2024-06-02 17:21:50 +0300

    Add files via upload

--- module2_3.py
+++ module2_3.py
@@ -1,56 +1,10 @@
 my_list = [42, 69, 0, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
-a = 0
-while a < my_list[0]:
-     print(my_list[0])
-     if a > my_list[0]:
-          break
-     if a < my_list[1]:
-          print(my_list[1])
-     if a > my_list[1]:
-          break
-     if a < my_list[2]:
-          print(my_list[2])
-     if a > my_list[2]:
-          break
-     if a < my_list[3]:
-          print(my_list[3])
-     if a > my_list[3]:
-          break
-     if a < my_list[4]:
-          print(my_list[4])
-     if a > my_list[4]:
-          break
-     if a < my_list[5]:
-          print(my_list[5])
-     if a > my_list[5]:
-          break
-     if a < my_list[6]:
-          print(my_list[6])
-     if a > my_list[6]:
-          break
-     if a < my_list[7]:
-          print(my_list[7])
-     if a > my_list[7]:
-          break
-     if a < my_list[8]:
-          print(my_list[8])
-     if a > my_list[8]:
-          break
-     if a < my_list[9]:
-          print(my_list[9])
-     if a > my_list[9]:
-          break
-     if a < my_list[10]:
-          print(my_list[10])
-     if a > my_list[10]:
-          break
-     if a < my_list[11]:
-          print(my_list[11])
-     if a > my_list[11]:
-          break
-     if a < my_list[12]:
-          print(my_list[12])
-     if a > my_list[12]:
-          break
-     if len(my_list[12] == 12):
+i = 0
+while my_list[i] >= 0:
+     if my_list[i] == 0:
+          i = i + 1
+          continue
+     print(my_list[i])
+     i = i + 1
+     if my_list[i] < 0:
           break
